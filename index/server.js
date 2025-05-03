c3aaca36c0ccb80676ef9dcd39e37fe2bb348ff4
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-05-03 16:28:44 -0300

    modificaoes

--- index/server.js
+++ index/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const port = process.env.PORT || 3000;
 dotenv.config();
 
 const app = express();
