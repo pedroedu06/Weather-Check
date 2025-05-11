034fa02f580fd0b7d298ca42d6303f72e8623ad4
pedroedu06 <pedro.nascimento182501@gmail.com>
2025-05-11 20:43:13 -0300

    updates

--- index/index.js
+++ index/index.js
@@ -1,6 +1,6 @@
 //dotenv
 async function getApiKey() {
-    const res = await fetch("/api-key");
+    const res = await fetch("https://weather-check-i9wh.onrender.com/api-key");
     const data = await res.json();
     return data.apiKey;
 }
